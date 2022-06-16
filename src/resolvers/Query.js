637a1b1bb1a1c5f08332a90a0f3a54b2e8955da3
VitalyyP <wisart@gmail.com>
2022-06-16 23:50:08 +0300

    fix

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,7 +2,6 @@ const { getPopular } = require("../modules/movies");
 
 async function movies(parent, args) {
   const data = await getPopular(args.page);
-  console.log(data.results.length);
   return data;
 }
 
