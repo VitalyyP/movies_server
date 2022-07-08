b807bd4f81b8495e620e5efc13338df39582712e
Vitalii Pikozh <wisart@gmail.com>
2022-07-08 23:44:11 +0300

    add query movie details

--- src/modules/movies/index.js
+++ src/modules/movies/index.js
@@ -10,6 +10,12 @@ const getPopular = async (page) => {
   return new Movies(result.data);
 };
 
+const getDetails = (id) =>
+  axios.get(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`
+  );
+
 module.exports = {
   getPopular,
+  getDetails,
 };
