a847d5be41948baf0e595446be0bf3bcc09b2ab3
Vitalii Pikozh <wisart@gmail.com>
2022-08-14 23:55:32 +0300

    add locale

--- src/modules/movies/index.js
+++ src/modules/movies/index.js
@@ -2,17 +2,17 @@ const axios = require("axios");
 const { Movies } = require("../entities/Movies");
 const { API_KEY } = require("../../../.env");
 
-const getPopular = async (page) => {
+const getPopular = async ({ page }, locale) => {
   const result = await axios.get(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`
+    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=${locale}&page=${page}`
   );
 
   return new Movies(result.data);
 };
 
-const getDetails = (id) =>
+const getDetails = (id, locale) =>
   axios.get(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=${locale}`
   );
 
 module.exports = {
